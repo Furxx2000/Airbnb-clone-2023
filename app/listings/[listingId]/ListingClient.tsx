a02a9a1e7f8bce78e75c82d6163fd0b0719f08bc
furxx2000 <furxx2000@gmail.com>
2023-06-09 00:29:38 +0800

    Finish individual listing view

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -7,10 +7,11 @@ import { SafeListing, SafeUser } from '@/app/types';
 import { categories } from '@/app/components/navbar/Categories';
 import Container from '@/app/components/Container';
 import ListingHead from '@/app/components/listings/ListingHead';
+import ListingInfo from '@/app/components/listings/ListingInfo';
 
 interface ListingClientProps {
   reservations?: Reservation[];
-  listing: SafeListing;
+  listing: SafeListing & { user: SafeUser };
   currentUser?: SafeUser | null;
 }
 
@@ -34,6 +35,17 @@ const ListingClient: React.FC<ListingClientProps> = ({
             locationValue={listing.locationValue}
             currentUser={currentUser}
           />
+          <div className='grid grid-cols-1 md:grid-cols-7 md:gap-10 mt-6'>
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              guestCount={listing.guestCount}
+              bathroomCount={listing.bathroomCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
