fa523cf8cf2813c0cdbc8b8b862ee810309a56e9
furxx2000 <furxx2000@gmail.com>
2023-06-15 07:17:12 +0800

    Change

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import getCurrentUser from './actions/getCurrentUser';
-import getListings, { IListingParams } from './actions/getListing';
-import Container from './components/Container';
-import EmptyState from './components/EmptyState';
-import ListingCard from './components/listings/ListingCard';
+import Container from '@/app/components/Container';
+import ListingCard from '@/app/components/listings/ListingCard';
+import EmptyState from '@/app/components/EmptyState';
+
+import getListings, { IListingParams } from '@/app/actions/getListing';
+import getCurrentUser from '@/app/actions/getCurrentUser';
 
 interface HomeProps {
   searchParams: IListingParams;
@@ -19,7 +20,7 @@ const Home = async ({ searchParams }: HomeProps) => {
   return (
     <Container>
       <div className='pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8'>
-        {listings.map((listing) => {
+        {listings.map((listing: any) => {
           return (
             <ListingCard
               currentUser={currentUser}
