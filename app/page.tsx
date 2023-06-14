9ec33dcf42f77275d21fdfd4a0686786ca53c153
furxx2000 <furxx2000@gmail.com>
2023-06-14 23:34:46 +0800

    Change to auto

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = 'force-static';
+export const dynamic = 'auto';
 
 import getCurrentUser from './actions/getCurrentUser';
 import getListings, { IListingParams } from './actions/getListing';
